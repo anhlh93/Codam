987f2b7a91389f34fb7ea6b384e2f3c629f7dfd6
Hai Anh Le <haile@f1r1s2.codam.nl>
2025-06-18 14:12:17 +0200

    Fix norminette

--- Circle 02/mini_talk/minitalk.h
+++ Circle 02/mini_talk/minitalk.h
@@ -6,16 +6,16 @@
 /*   By: haile <haile@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/06/18 12:24:32 by haile         #+#    #+#                 */
-/*   Updated: 2025/06/18 14:04:22 by haile         ########   odam.nl         */
+/*   Updated: 2025/06/18 14:11:58 by haile         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-#include <unistd.h>     // For getpid(), usleep(), pause()
-#include <stdlib.h>     // For malloc(), free(), exit(), EXIT_FAILURE
-#include <signal.h>     // For signal handling (kill, sigaction, SIGUSR1, SIGUSR2)
+# include <unistd.h>     // For getpid(), usleep(), pause()
+# include <stdlib.h>     // For malloc(), free(), exit(), EXIT_FAILURE
+# include <signal.h>     // For signal handling
 # include "ft_printf/ft_printf.h"
 
 /* STRUCTURE*/
@@ -24,7 +24,7 @@ typedef struct s_struct
     char    c;
     char    *message;
     pid_t   pid_client;
-}       t_struct;
+}   t_struct;
 
 /* CLIENT */
 
