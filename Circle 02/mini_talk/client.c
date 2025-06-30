76cff588fa169a014363aa242fff2f276e3b80fb
Hai Anh Le <haile@f1r1s9.codam.nl>
2025-06-30 14:56:41 +0200

    Update last

--- Circle 02/mini_talk/client.c
+++ Circle 02/mini_talk/client.c
@@ -6,95 +6,100 @@
 /*   By: haile <haile@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/06/18 12:29:34 by haile         #+#    #+#                 */
-/*   Updated: 2025/06/18 13:25:37 by haile         ########   odam.nl         */
+/*   Updated: 2025/06/30 13:12:17 by haile         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-unsigned int bits_send = 0;
+unsigned int	g_bitsend = 0;
 
-int ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-    int res;
-    int sign;
+	int	res;
+	int	sign;
 
-    res = 0;
-    sign = 1;
-    while ((*str >=  9 && *str <= 13) || *str == 32)
-        str++;
-    if (*str == '+' || *str == '-')
-    {
-        if (*str == '-')
-            sign *= -1;
-        str++;
-    }
-    while (*str >= '0' && *str <= '9')
-    {
-        res = res * 10 + (*str - '0');
-        str++;
-    }
-    return (res * sign);
-}
-void signal_to_client(int signum)
-{
-    if (signum == SIGUSR1)
-        ft_printf("%d bits succesfully sent", bits_send);
+	res = 0;
+	sign = 1;
+	while ((*str >= 9 && *str <= 13) || *str == 32)
+		str++;
+	if (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			sign *= -1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		res = res * 10 + (*str - '0');
+		str++;
+	}
+	return (res * sign);
 }
 
-void ft_error(int i)
+void	signal_to_client(int signum)
 {
-    if (i == 1)
-        ft_printf("ERROR: Invalid argument");
-    else if (i == 2)
-        ft_printf("ERROR: Non - positive PID");
-    else if (i == 3)
-        ft_printf("ERROR: Sigaction error");
-    else if (i == 4)
-        ft_printf("ERROR: kill function error");
-    exit (EXIT_FAILURE);
+	if (signum == SIGUSR1)
+		ft_printf("%d bits succesfully sent", g_bitsend);
 }
-void signal_to_server(char s, int pid)
-{
-    int i;
 
-    i = 7;
-    while (i > -1)
-    {
-        if (s & 1 << i)
-        {
-            if (kill(pid, SIGUSR1) != 0)
-                ft_error(4);
-        }
-        else
-        {
-            if (kill(pid, SIGUSR2) != 0)
-                ft_error(4);
-        }
-        usleep(300);
-        i--;
-    }
+void	ft_error(int i)
+{
+	if (i == 1)
+		ft_printf("ERROR: Invalid argument");
+	else if (i == 2)
+		ft_printf("ERROR: Non - positive PID");
+	else if (i == 3)
+		ft_printf("ERROR: Sigaction error");
+	else if (i == 4)
+		ft_printf("ERROR: kill function error");
+	exit(EXIT_FAILURE);
 }
-int main(int ac, char **av)
-{
-    int i;
-    int   pid_server;
-    struct sigaction    sa;
 
-    if (ac != 3)
-        ft_error(1);
-    sa.sa_handler = signal_to_client;
-    i = 0;
-    pid_server = ft_atoi(av[1]);
-    if (pid_server <= 0)
-        ft_error(2);
-    if (sigaction(SIGUSR1, &sa, NULL) == -1)
-        ft_error(3);
-    while (av[2][i])
-    {
-        signal_to_server(av[2][i], pid_server);
-        i++;
-        bits_send += 8;
-    }
-    return (0);
+void	signal_to_server(char s, int pid)
+{
+	int	i;
+
+	i = 7;
+	while (i > -1)
+	{
+		if (s & 1 << i)
+		{
+			if (kill(pid, SIGUSR1) != 0)
+				ft_error(4);
+		}
+		else
+		{
+			if (kill(pid, SIGUSR2) != 0)
+				ft_error(4);
+		}
+		usleep(900);
+		i--;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	int					i;
+	int					pid_server;
+	struct sigaction	sa;
+
+	if (ac != 3)
+		ft_error(1);
+	sa.sa_handler = signal_to_client;
+	i = 0;
+	pid_server = ft_atoi(av[1]);
+	if (pid_server <= 0)
+		ft_error(2);
+	if (sigaction(SIGUSR1, &sa, NULL) == -1)
+		ft_error(3);
+	while (av[2][i])
+	{
+		signal_to_server(av[2][i], pid_server);
+		i++;
+		g_bitsend += 8;
+	}
+	signal_to_server('\0', pid_server);
+	g_bitsend += 8;
+	return (0);
 }
--- Circle 02/mini_talk/server.c
+++ Circle 02/mini_talk/server.c
@@ -6,96 +6,97 @@
 /*   By: haile <haile@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/06/18 13:27:59 by haile         #+#    #+#                 */
-/*   Updated: 2025/06/18 14:14:42 by haile         ########   odam.nl         */
+/*   Updated: 2025/06/30 13:13:33 by haile         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-t_struct    mes;
+t_struct	g_mes;
 
-char    *ft_charjoin(char *s1, char s2)
+char	*ft_charjoin(char *s1, char s2)
 {
-    int size;
-    int i;
-    char    *new;
+	int		size;
+	int		i;
+	char	*new;
 
-    i = 0;
-    size = 0;
-    while (s1 && s1[size])
-        size++;
-    new = malloc(sizeof(char) * (size + 2));
-    if (new == NULL)
-        return (NULL);
-    while (s1 && s1[i])
-    {
-        new[i] = s1[i];
-        i++;
-    }
-    new[i++] = s2;
-    new[i] = '\0';
-    free(s1);
-    return (new);
+	i = 0;
+	size = 0;
+	while (s1 && s1[size])
+		size++;
+	new = malloc(sizeof(char) * (size + 2));
+	if (new == NULL)
+		return (NULL);
+	while (s1 && s1[i])
+	{
+		new[i] = s1[i];
+		i++;
+	}
+	new[i++] = s2;
+	new[i] = '\0';
+	free(s1);
+	return (new);
 }
 
-void    ft_error(int i)
+void	ft_error(int i)
 {
-    if (i == 1)
-        ft_printf("ERROR: Invalid argument");
-    else if (i == 2)
-        ft_printf("ERROR: Non - positive PID");
-    else if (i == 3)
-        ft_printf("ERROR: Sigaction error");
-    else if (i == 4)
-        ft_printf("ERROR: kill function error");
-    exit (EXIT_FAILURE);
-}
-void    ft_handler(int signum, siginfo_t *pid_client, void *tmp)
-{
-    static int i = 0;
-
-    (void)tmp;
-    mes.pid_client = pid_client->si_pid;
-    if (signum == SIGUSR1)
-    {
-        mes.c <<= 1;
-        mes.c |= 1;
-    }
-    else
-        mes.c <<= 1;
-    i++;
-    if (i == 8)
-    {
-        mes.message = ft_charjoin(mes.message, mes.c);
-        if (!mes.c)
-        {
-            ft_printf("%s\n",mes.message);
-            free(mes.message);
-            mes.message = NULL;
-            kill(mes.pid_client, SIGUSR1);
-            usleep(300);
-        }
-        i = 0;
-    }
+	if (i == 1)
+		ft_printf("ERROR: Invalid argument");
+	else if (i == 2)
+		ft_printf("ERROR: Non - positive PID");
+	else if (i == 3)
+		ft_printf("ERROR: Sigaction error");
+	else if (i == 4)
+		ft_printf("ERROR: kill function error");
+	exit (EXIT_FAILURE);
 }
 
-int main(void)
+void	ft_handler(int signum, siginfo_t *pid_client, void *tmp)
 {
-    struct sigaction    sa;
-    int                 pid;
+	static int	i = 0;
 
-    pid = getpid();
-    ft_printf("Current PID of server: %d", pid);
-    sa.sa_sigaction = ft_handler;
-    sa.sa_flags = SA_SIGINFO;
-    mes.message = NULL;
-    while (12)
-    {
-        if (sigaction(SIGUSR1, &sa, NULL) == -1)
-            ft_error(3);
-        if (sigaction(SIGUSR2, &sa, NULL) == -1)
-            ft_error(3);
-    }
-    pause();
-    return (0);
+	(void)tmp;
+	g_mes.pid_client = pid_client->si_pid;
+	if (signum == SIGUSR1)
+	{
+		g_mes.c <<= 1;
+		g_mes.c |= 1;
+	}
+	else
+		g_mes.c <<= 1;
+	i++;
+	if (i == 8)
+	{
+		g_mes.message = ft_charjoin(g_mes.message, g_mes.c);
+		if (!g_mes.c)
+		{
+			ft_printf("%s\n", g_mes.message);
+			free(g_mes.message);
+			g_mes.message = NULL;
+			kill(g_mes.pid_client, SIGUSR1);
+			usleep(900);
+		}
+		i = 0;
+	}
+}
+
+int	main(void)
+{
+	struct sigaction	sa;
+	int					pid;
+
+	pid = getpid();
+	ft_printf("Current PID of server: %d\n", pid);
+	sa.sa_sigaction = ft_handler;
+	sa.sa_flags = SA_SIGINFO;
+	g_mes.message = NULL;
+	if (sigaction(SIGUSR1, &sa, NULL) == -1)
+		ft_error(3);
+	if (sigaction(SIGUSR2, &sa, NULL) == -1)
+		ft_error(3);
+	while (12)
+	{
+		pause();
+	}
+	return (0);
 }
